6a714c808803c152428ba059ed36cceb3669b515
Laszlo Foldi <laszlo.foldi@attentioncrm.com>
2026-03-14 12:47:05 +0100

    Change favicon from svg to png

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
     base: '/pages-demo/',
-    title: "Docs Demo",
-    description: "Demo for creating public static github pages with VitePress.",
+    title: 'Docs Demo',
+    description: 'Demo for creating public static github pages with VitePress.',
     appearance: true,
     head: [
         [ 'link', { rel: 'stylesheet',  href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap' } ],
-        ['link', { rel: 'icon', href: '/fav_icon.svg' }]
+        [ 'link', { rel: 'icon', href: '/fav_icon.png' } ]
     ]
 })
